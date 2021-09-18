b74d52976781b460493f451b7bc8167973412bcc
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-09-18 16:56:09 +0300

    Add files via upload

---
+++ ChromeDuplicatingFilesPrank/Chrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace Chrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+
+            Random generator = new Random();
+            
+            string file = "begin.zip";
+
+            string directory = @"D:\Documents\SOFTUNI\algorithms";
+
+            string source = Path.Combine(directory, file);
+
+            string destination = Path.Combine(directory, generator.Next().ToString());
+
+            File.Copy(source, destination);
+        }
+    }
+}
