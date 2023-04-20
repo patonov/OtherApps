48207e6353b6ad0ec87f5042f2738b825fcb6845
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-04-20 17:27:48 +0300

    Add files via upload

---
+++ ElfWitthWords/ElfWitthWords/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ElfWitthWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstWord = Console.ReadLine()!.Split(" ").ToArray();
+            var secondWord = Console.ReadLine()!.Split(" ").ToArray();
+            int result = 0;
+
+            for (int i = 0; i < firstWord.Count(); i++) 
+            {
+                var letter = firstWord[i];
+
+                for (int j = 0; j < secondWord.Count(); j++)
+                {
+                    if (secondWord[j] == letter)
+                    {
+                        secondWord[j] = "_";
+                        firstWord[i] = "_";
+                    }
+                }
+
+            }
+
+            result += firstWord.Count(x => x == "_");
+            result  += secondWord.Count(x => x == "_");
+            Console.WriteLine(result);
+        }
+    }
+}
