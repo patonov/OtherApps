81aaeb81c9767c9490882f931874363aced91c63
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-04-24 18:08:51 +0300

    Add files via upload

---
+++ BallsInBox/BallsInBox/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace BallsInBox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> collorBalls = new Dictionary<string, int>();
+   
+            string input = Console.ReadLine()!;
+
+            while (true)
+            {
+                if (input == "End")
+                {
+                    break;
+                }
+
+                var inputArr = input.Split(": ").ToArray();
+                string color = inputArr[0];
+                int balls = int.Parse(inputArr[1]);
+
+                if (!collorBalls.ContainsKey(color))
+                {
+                    collorBalls.Add(color, 0);
+                    collorBalls[color] = balls;
+                }
+                else
+                {
+                    collorBalls[color] += balls;
+                }
+
+                input = Console.ReadLine()!;
+            }
+
+            
+            int interim = 0;
+
+            foreach (var color in collorBalls)
+            {               
+                interim += color.Value;
+            }
+
+            int ballsToTakeOut = interim - 1;
+
+            Console.WriteLine($"We should take {ballsToTakeOut} balls to be sure there is only one ball in the box. One ball garantees one color left");
+
+
+        }
+
+        
+
+    }
+}
